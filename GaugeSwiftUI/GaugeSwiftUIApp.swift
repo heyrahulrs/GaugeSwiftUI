888940b9fda1eaf9df0a5432550604d57d00f54f
Rahul Sharma <rahulrs0029@gmail.com>
2022-03-04 16:14:54 +0530

    Initial Commit

---
+++ GaugeSwiftUI/GaugeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeSwiftUIApp.swift
+//  GaugeSwiftUI
+//
+//  Created by Rahul Sharma on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GaugeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
